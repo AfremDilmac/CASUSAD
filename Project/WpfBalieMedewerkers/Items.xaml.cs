b6caf1e9d5ec9fbf6fa10659b0eabebd9f6282d6
Afrem <71547964+AfremDilmac@users.noreply.github.com>
2021-05-25 17:35:28 +0200

    commit

--- Project/WpfBalieMedewerkers/Items.xaml.cs
+++ Project/WpfBalieMedewerkers/Items.xaml.cs
@@ -27,58 +27,42 @@ namespace WpfBalieMedewerkers
         string connString = ConfigurationManager.AppSettings["connString"];
         public Items()
         {
-            ReloadEmployees(null);
+           // Reload(null);
             InitializeComponent();
-            string[] files = Directory.GetFiles("../../Image", "*.jpg"); // startfolder = bin/Debug/
-            for (int i = 0; i < files.Length; i++)
-            {
-                // get file url
-                string filename = files[i];
-                FileInfo fi = new FileInfo(filename);
+            ReloadEmployees(null);
 
-                // create image and label
-                Image img = new Image();
-                img.Width = 80;
-                img.Source = new BitmapImage(new Uri(filename, UriKind.Relative));
-                Label lbl = new Label();
-                lbl.Content = $"photo {i + 1}";
 
-                // create stackpanel
-                StackPanel pnl = new StackPanel();
-                pnl.Margin = new Thickness(0, 0, 20, 20);
-                pnl.Children.Add(img);
-                pnl.Children.Add(lbl);
-
-                // add stackpanel to wrappanel
-                wrpPanel.Children.Add(pnl);
-            }
         }
 
-        public void Reload(int? selectedId)
+        public void ReloadEmployees(int? selectedId)
         {
-            // wis lijst en labels
-            lbxResults.Items.Clear();
+            // wis de lijst
+            lstBox.Items.Clear();
 
             // laad alle werknemers in
-            List<Items> allEmps = new List<Items>();
-            foreach (Items emp in allEmps)
+            List<BibItems> allEmps = BibItems.GetAll();
+            foreach (BibItems emp in allEmps)
             {
                 ListBoxItem item = new ListBoxItem();
                 item.Content = emp.ToString();
                 item.Tag = emp.Id;
                 item.IsSelected = selectedId == emp.Id;
-                lbxResults.Items.Add(item);
+                lstBox.Items.Add(item);
             }
-        }
-
-        private void btnHome_Click(object sender, RoutedEventArgs e)
-        {
-
         }
 
         private void lbxResults_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            ListBoxItem li = (ListBoxItem)lstBox.SelectedItem;
+            if (li == null) return;
+            int id = (int)li.Tag;
+            BibItems mwd = BibItems.GetById(id);
+            lblTitel.Content = mwd.Titel;
+            lblBeschrijving.Content = mwd.Beschrijving;
+            lblUitgeverij.Content = mwd.Uitgeverij;
+            lblLeeftijdVan.Content = mwd.Leeftijd_van;
+            lblLeeftijdTot.Content = mwd.Leeftijd_tot;
+            lblTaal.Content = mwd.Taal;
         }
     }
 }
